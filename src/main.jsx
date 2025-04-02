77fd983baeb153138ddb9ccfcf92b21f1bcb4966
Usama Marfani <84830008+usamamarfani@users.noreply.github.com>
2025-04-02 13:13:27 +0100

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import UsamaPortfolio from './UsamaPortfolio.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <UsamaPortfolio />
-  </React.StrictMode>
-);
